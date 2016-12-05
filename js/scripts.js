338916353c8b9bf6639085a1cd0b9e169d53afed
sking24 <sking24@eagles.ewu.edu>
2016-12-04 23:48:30 -0800

    updated file, add js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+  $(document).ready(function(){
+        $('#blindify').blindify({
+            numberOfBlinds: 10,
+            animationSpeed: 600,
+            delayBetweenSlides: 200,
+            slideVisibleTime: 3000,
+            width: 960,
+            height: 600
+        });
+    });
+
