7bdda8264efd0b0eb131e7f8d8bf0f3a6b40949b
sking24 <sking24@eagles.ewu.edu>
2016-12-05 14:46:34 -0800

    add images & body info

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,16 @@
             animationSpeed: 600,
             delayBetweenSlides: 200,
             slideVisibleTime: 3000,
-            width: 900,
-            height: 450,
+            width: 780,
+            height: 550,
            
         });
     });
 
+// Intialize Masonry
+$('article').masonry({
+	itemSelector: 'section',
+	columnWidth: 200,
+	gutter: 10,
+	FitWidth: true
+});
